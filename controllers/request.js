554341bbb1bb32bb75e60526e8e491c6ad9baf5b
Ilya Tate <ilyatate@gmail.com>
2021-11-19 13:38:44 -0700

    Maybe finished request controller

--- controllers/request.js
+++ controllers/request.js
@@ -2,17 +2,22 @@ const mongoose = require("mongoose");
 const { StatusCodes } = require("http-status-codes");
 
 const { Request } = require("../models");
-const { NotFoundError } = require("../errors");
+const { BadRequestError, NotFoundError } = require("../errors");
 
-const getRequest = async (req, res) => {
+const findRequestById = (req) => {
   const { id: requestId } = await req.params;
-  const request = Request.findOne({
+  const request = Request.findById({
     _id: requestId
   });
-
-  if (!job) {
-    throw new NotFoundError(`Request of id ${requestId} not found`);
+  if (!request) {
+    throw new NotFoundError(`No request of id ${requestId}`);
   }
+
+  return request;
+};
+
+const getRequest = async (req, res) => {
+  const request = findRequestById(req);
 
   res.status(200).json({ request });
 };
@@ -24,16 +29,36 @@ const postRequest = async (req, res) => {
   const request = await req.body;
   res.status(StatusCodes.CREATED).json({ request });
 };
-const removeRequest = (req, res) => {
-  res.json({ msg: "Me", status: 200 });
+const removeRequest = async (req, res) => {
+  const request = findRequestById(req);
+
+  res.status(200).json({ request });
 };
-const updateRequest = (req, res) => {
-  res.json({ msg: "The", status: 200 });
+const updateRequest = async (req, res) => {
+  const {
+    body: { petID: petId, status },
+    user: { userID: userId },
+    params: { id: requestId }
+  } = req;
+  if (!petId || !status) {
+    throw new BadRequestError("Pet ID and status required");
+  }
+  if (!requestId) {
+    throw new NotFoundError(`No request of id ${requestId}`);
+  }
+
+  const request = await Request.findByIdAndUpdate({ id: requestId }, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({ request });
 };
+
 module.exports = {
   postRequest,
   getRequest,
-  getRequests,
+  getAllRequests,
   removeRequest,
   updateRequest
 };
