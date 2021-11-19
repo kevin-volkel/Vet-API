8a423c39b400c6bc55486d7126bb18472fb7931d
Ilya Tate <ilyatate@gmail.com>
2021-11-19 15:19:43 -0700

    Completed adopted controller

--- controllers/adopted.js
+++ controllers/adopted.js
@@ -1,24 +1,55 @@
-const mongoose = require('mongoose');
-const { StatusCodes } = require('http-status-codes');
+const mongoose = require("mongoose");
+const { StatusCodes } = require("http-status-codes");
 
-const { Adopted } = require('../models');
+const { Adopted } = require("../models");
 
-const getAdopted = (req, res) => {
-  res.json({ msg: 'Gonna', status: 200 });
+const findAdoptedById = async (req) => {
+  const { id: adoptedId } = await req.params;
+  const adopted = Request.findById({
+    _id: adoptedId
+  });
+  if (!adopted) {
+    throw new NotFoundError(`No adopted of id ${adoptedId}`);
+  }
+
+  return adopted;
+};
+const checkPermissionIsUser = async (req) => {
+  if (req.user.permission === "user") {
+    throw new UnauthError("Permission denied");
+  }
+};
+
+const getAdopted = async (req, res) => {
+  const adopted = await findAdoptedById(req);
+  res.status(200).json({ adopted });
 };
 const getAllAdopted = async (req, res) => {
-  const adopted = await Adopted.sort('adoptee');
-  res.status(StatusCodes.OK).json({ adopted, count: adopted.length });
+  const adopted = await Adopted.find({});
+  res.status(200).json({ adopted, count: adopted.length });
 };
 const postAdopted = async (req, res) => {
-  const adoption = await req.body;
-  res.status(StatusCodes.CREATED).json({ adoption });
+  await checkPermissionIsUser(req);
+  const adopted = await Adopted.create(req.body);
+  res.status(StatusCodes.CREATED).json({ adopted });
 };
-const removeAdopted = (req, res) => {
-  res.json({ msg: 'Gonna', status: 200 });
+const removeAdopted = async (req, res) => {
+  await checkPermissionIsUser(req);
+  const adopted = Adopted.findByIdAndDelete(req.params.id);
+  res.status(200).json({ request });
 };
-const updateAdopted = (req, res) => {
-  res.json({ msg: 'Gonna', status: 200 });
+const updateAdopted = async (req, res) => {
+  await checkPermissionIsUser(req);
+  const { id: adoptedId } = req.params;
+  const adopted = await Adopted.findByIdAndUpdate(adoptedId, req.body, {
+    new: true,
+    runValidators: true
+  });
+  if (!adopted) {
+    throw new NotFoundError(`No adopted of id ${adoptedId}`);
+  }
+
+  res.status(200).json({ adopted });
 };
 
 module.exports = {
@@ -26,5 +57,5 @@ module.exports = {
   getAllAdopted,
   postAdopted,
   removeAdopted,
-  updateAdopted,
+  updateAdopted
 };
